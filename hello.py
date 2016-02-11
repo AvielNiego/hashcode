aab949e23835c0c468777441f0b8e002fc7b5e2f
yakki <yakkitoledano@gmail.com>
2016-02-11 22:01:29 +0200

    ads

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
+def parse_warehouse(input_file):
+    return [(input_file.readline().split()), enumerate(input_file.readline().split())]
 
-print "hello world"
+
+input_file = open("D:\\Projects\\hashcode\\resources\\busy_day.in")
+
+line = input_file.readline()
+(rows, colms, drones_count, deadline, max_weight) = line.split()
+qwe, asd = [1, 1]
+
+product_count = input_file.readline()
+products = enumerate(input_file.readline().split())
+
+warehouses_count = input_file.readline()
+
+warehouses = [parse_warehouse(input_file) for _ in range(int(warehouses_count))]
