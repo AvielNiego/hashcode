2de56fc1257efe64c5d9c0def402a6ccd40afd56
AvielNiego <avielniego@gmail.com>
2016-02-11 22:40:01 +0200

    build graph

--- algo/make_graph.py
+++ algo/make_graph.py
@@ -1,17 +1,19 @@
+import math
+
 import src.busy_parser
 from src import busy_parser
 
 
-def distance(v1, v2):
-    return 1
+def distance(ver1, ver2):
+    return math.sqrt(math.pow(ver1.location[0] - ver2.location[0], 2) + math.pow(ver1.location[1] - ver2.location[1], 2))
 
 products = busy_parser.products
 warehouses = busy_parser.warehouses
-orders = []
+costumers = busy_parser.costumers
 
-g = dict.fromkeys(["w" + str(w) for w in xrange(len(warehouses))], [])
-g.update(dict.fromkeys(["o" + str(w) for w in xrange(len(orders))], []))
+g = dict.fromkeys([w for w in warehouses], [])
+g.update(dict.fromkeys([c for c in costumers], []))
 
 
-for v1 in g:
-    g[v1] = [(v2, distance(v1, v2)) for v2 in g]
+for v1 in g.keys():
+    g[v1] = [(v2, distance(v1, v2)) for v2 in g.keys()]
